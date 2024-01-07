3797540546855ab73efba905e2f5f1eca8cdefa4
Suraj saini <145084384+surajmusicbots@users.noreply.github.com>
2024-01-07 09:30:50 +0530

    Update git.py

--- DAXXMUSIC/core/git.py
+++ DAXXMUSIC/core/git.py
@@ -33,7 +33,7 @@ def git():
     REPO_LINK = config.UPSTREAM_REPO
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
-        TEMP_REPO = REPO_LINK.split("https://")[1]
+        TEMP_REPO = REPO_LINK.split("https://github.com/surajmusicbots/MUSIC")[1]
         UPSTREAM_REPO = f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
